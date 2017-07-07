b2a128a0303a783986cfe2b6736310d3d00156c0
PERKINELMERNET\GaoXz <xing.gao@perkinelmer.com>
2017-07-07 16:41:09 +0800

    Using ES6 rewrite the Like UnLike function

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom'
 import './app.css'
 
 
-var Likebutton =React.createClass({
-    componentWillReceiveProps:function(nextPorops){       
-        // console.log(nextPorops)
-        // console.log("this only exist in child componet, During the Page runing ")
-    },
-    getInitialState:function(){
-        return {
-            liked:false
+class Likebutton extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            liked: props.liked||false
         }
-    },
-    handleClick:function(){
+        this.handleClick =this.handleClick.bind(this)
+    }
+
+    handleClick(){
         if(this.state.liked){
             this.props.unLike();
             this.setState({
@@ -25,54 +24,39 @@ var Likebutton =React.createClass({
                 liked:true
             })
         }
-    },
-    render:function(){
-        // console.log("Child component Rendering")
+    }
+    render(){
          if(!this.state.liked)
             return (           
                 <div><button onClick ={this.handleClick} className="mybutton">Like</button></div>
             )
         else{
             return(
-                <div><button onClick ={this.handleClick} className="mybutton">disLike</button></div>
+                <div><button onClick ={this.handleClick} className="mybutton">unLike</button></div>
             )
         }
     }
-    
-})
-var Lifecyle =React.createClass({
-    getDefaultProps:function(){
-        // console.log("Here is the LifeCyle of init React component")
-        // console.log('1st-step-- getDefaultProps, Get Default Props from parent Node');
-        return {
-            name :'nodeBook'
-        }
-    },
-    getInitialState:function(){
-        // console.log('2nd-step-- getInitialState, Get Or Set up component initial or default state');
-        return {
+}
+class Lifecyle extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
             like:0
         }
-    },
-    addLikes:function(){
+        this.addLikes=this.addLikes.bind(this);
+        this.removeLikes =this.removeLikes.bind(this);
+    } 
+    addLikes(){
         this.setState({
             like :this.state.like+1
         });
-    },
-    removeLikes:function(){
+    }
+    removeLikes(){
         this.setState({
             like :this.state.like-1
         });
-    },
-    componentWillMount:function(){
-        //console.log('3rd-step-- componentWillMount, Before Render, you can Change your state, e.g. here, I set the default Like =100');
-        this.setState({
-            like :this.state.like+100
-        })
-    }, 
-    render:function(){
-       // console.log('4th-step-- render, Here We go, Render in the Virtual DOM');
-       // console.log("Rendering Rendering");
+    }
+    render(){    
         return(
         <div>
             <p><strong>This File is put here to let you understand the Life Cyle of React, Please press F12 to see the log lines</strong></p>
@@ -82,25 +66,8 @@ var Lifecyle =React.createClass({
             <Likebutton likeHandle={this.addLikes} unLike={this.removeLikes}/>
         </div>
         )
-    },
-    componentDidMount:function(){
-       // console.log('5th-step-- componentDidMount, after you have render in the Virtual DOM, you could doing someting here')
-    },
-    shouldComponentUpdate:function(nextPorops,nextState){ 
-       // console.log("During the Page Runing, this is the 1st step. which checked the state and props, just checking, can not modify them"); 
-        //this step is always used to check up the Username or other things 
-        return nextPorops.name===this.props.name;
-    },
-    componentWillUpdate:function(){
-        // console.log("During the Page Runing, this is the 2nd step, this component will update")
-    },
-    componentDidUpdate:function(){
-         //console.log("During the Page Runing, this is the 4th step, this component has updated")
-    },
-    componentWillUnmount:function(){
-       // console.log("this componet is being Destroyed")
     }
-})
-ReactDOM.render(<Lifecyle/>,document.getElementById('root'),function(){
+}
+ReactDOM.render(<Lifecyle name ="nodeBook"/>,document.getElementById('root'),function(){
     //console.log('6th-step--ReacDOM.render, this is the last Step, to Render Element in the real DOM tree')
 })
